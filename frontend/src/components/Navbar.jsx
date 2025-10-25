43e77d81cbb4431d5f0c84c30f0645179a675323
Sree Lakshmi Dileep <sreelakshmidileep2004@gmail.com>
2025-10-25 11:52:42 +0530

    navbar UI

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <div style={{ backgroundColor: "#097531", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px 20px",height:"6rem" }}>
+        
+        <div>
+          <h2 style={{ color: "#fff", margin: 0 ,fontSize:"2.8rem"}}>TastyQuest</h2>
+        </div>
+
+        
+        <div style={{ display: "flex", gap: "60px",marginRight:"4rem" }}>
+          <Link to="/" style={{ color: "#fff", textDecoration: "none",fontSize:"1.8rem" }}>Home</Link>
+          <Link to="/recipes" style={{ color: "#fff", textDecoration: "none",fontSize:"1.8rem" }}>Recipes</Link>
+          <Link to="/about" style={{ color: "#fff", textDecoration: "none",fontSize:"1.8rem" }}>About</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to TastyQuest! </h1>
+        <h3>Discover recipes that match your cravings — quick, delicious, and made just for you.</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Home
